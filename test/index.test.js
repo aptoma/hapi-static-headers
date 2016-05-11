531867e19b38e8b15407144579fcdb69a9b2671d
Gunnar Lium <gunnar@aptoma.com>
2016-05-11 09:32:03 +0200

    Add support for adding headers to Boom responses
    The response object for Boom errors is different from normal response
    objects, meaning among other things that `.headers()` is not available.
    The same effect can be achieved by accessing `response.output.headers`
    instead.

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('hapi');
 const assert = require('chai').assert;
+const Boom = require('boom');
 
 describe('Hapi Static Headers', () => {
 	it('should require object option', (done) => {
@@ -40,6 +41,33 @@ describe('Hapi Static Headers', () => {
 					.then((res) => {
 						assert(res.headers.foo === 'bar');
 						assert(res.headers.boo === 'foobar');
+						done();
+					});
+			})
+			.catch(done);
+	});
+
+	it('should add headers to error responses', (done) => {
+		const server = new Hapi.Server({debug: false});
+		server.connection();
+
+		server.register({
+			register: require('../'),
+			options: {
+				headers: {
+					foo: 'bar'
+				}
+			}
+		})
+			.then(() => {
+				server.route({method: 'GET', path: '/', handler: (req, reply) => reply(Boom.badData('foo'))});
+				server
+					.inject({
+						url: '/',
+						method: 'GET'
+					})
+					.then((res) => {
+						assert(res.headers.foo === 'bar');
 						done();
 					});
 			})
